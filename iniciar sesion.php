2d45cf5d51f101df0ac34a9ef9b82bdb9e6713b8
RuyFr <82530881+RuyFr@users.noreply.github.com>
2021-05-12 19:13:49 -0500

    Create iniciar sesion.php

---
+++ iniciar sesion.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>
+            Registro
+        </title>
+        
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="estilo.css">
+    </head>
+    <body>
+        <div><header id="encabezado">
+            <a id="logo" href="#">
+                <img src="logo coacalco.png" href="https://coacalco.gob.mx/">
+                <span><h3>PROGRAMAS DE BIENESTAR COACALCO</h3></span>
+            </a>
+            <div><nav>
+                <ul>
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="#">Registrarse</a></li>
+                    <li><a href="iniciar sesion.php">Iniciar Sesión</a></li>
+                </ul>
+            </nav>
+        </header>
+        </div>
+        </div>
+        <section id="formulario">
+            <h3><center>Iniciar Sesión</center></h3>
+            <input class="controls" type="email" name="correo" id="correo" placeholder="Correo electronico">
+            <input class="controls" type="password" name="correo" id="correo" placeholder="Contraseña">
+            <p> <a href="#">Iniciar Sesion</a></p>
+        </section>
+    </body>
+</html>
